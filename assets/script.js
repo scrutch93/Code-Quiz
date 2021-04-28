c4e5b3da4dedcea553c03e5b0bce80755b0fa74a
Steven Crutchfield <stevccru@icloud.com>
2021-04-27 23:06:18 -0400

    added timer

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,26 @@
-const startButton = document.getElementById("begin-button");
+var timeLeft = 10;
+/*var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+  }
+  /*document.getElementById("progressBar").value = 10 - timeleft;
+  timeleft -= 1;
+}, 1000);*/
 
 
-startButton.addEventListener('click', startgame);
 
+function startGame(firstName){
+console.log(firstName);
+var timeLeft = 180;
+document.getElementById("timer").innerHTML = timeLeft;
 
-function startgame(){
-console.log("Game has begun!")
+var startTimer = setInterval(function(){
+    if(timeleft <= 0){
+      clearInterval(startTimer);
+    }
+    document.getElementById("progressBar").value = 10 - timeleft;
+    timeleft -= 1;
+  }, 1000);
+
 }
+
