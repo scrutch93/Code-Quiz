6ca57252aee4674f0c77f74e3ce56233e887832e
Steven Crutchfield <stevccru@icloud.com>
2021-04-28 01:41:24 -0400

    added more switch cases

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,27 @@
-function startGame(){    
+function startGame() {
     document.getElementById("begin-button").style.display = "none";
     var timeLeft = 10;
-    
-    setInterval(function(){    
-        if(timeLeft <= 0){
+
+    setInterval(function () {
+        if (timeLeft <= 0) {
             clearInterval(startTimer);
-          }
+        }
         document.getElementById("timer").innerHTML = timeLeft;
-        timeLeft -= 1;      
+        timeLeft -= 1;
     }, 1000);
 
     console.log(newQuestion(1));
-    document.getElementById("question").innerHTML=newQuestion("question1");
+    document.getElementById("question").innerHTML = newQuestion("question1");
 }
 
 
-function newQuestion(questionNumber){
-    var question1 = "What is an example of a boolean?"
-    var question2 = "What is an exmaple of a string?"
-    var question3 = "Select an example of a function"
-    var question4 = "Select an exmaple of an array"
-    var question5 = "How would you replace a text in a DOM element?"
-    
+function newQuestion(questionNumber) {
+    var question1 = "What is an example of a boolean?";
+    var question2 = "What is an exmaple of a string?";
+    var question3 = "Select an example of a function";
+    var question4 = "Select an exmaple of an array";
+    var question5 = "How would you replace a text in a DOM element?";
+
     switch (questionNumber) {
         case "question1":
             return question1;
@@ -45,43 +45,58 @@ function newQuestion(questionNumber){
 }
 
 function newAnswers(questionNumber) {
-    switch(questionNumber){
+
+    switch (questionNumber) {
         case "question1":
-
+            var question1Answer1 = "var grape = true";
+            var question1Answer2 = "[Apple, Orange, Bannanas]";
+            var question1Answer3 = "3";
+            var question1Answer4 = "5 + 6";
+            break;
         case "question2":
+            var question2Answer1 = "9870897";
+            var question2Answer2 = "['cats', 'dogs', 'fish', 'cows']";
+            var question2Answer3 = "false";
+            var question2Answer4 = "'jquery'";
 
+            // do stuff
+            break;
         case "question3":
-
+            var question3Answer1 = "displaySolutions(x,y)";
+            var question3Answer2 = "['cats', 'dogs', 'fish', 'cows']";
+            var question3Answer3 = "false";
+            var question3Answer4 = "'jquery'";
+            // do stuff
+            break;
         case "question4":
+            // do stuff
+            var question4Answer1 = "displaySolutions(x,y)";
+            var question4Answer2 = "['cats', 'dogs', 'fish', 'cows']";
+            var question4Answer3 = "false";
+            var question4Answer4 = "'jquery'";
 
+            break;
         case "question5":
+            var question5Answer1 = "displaySolutions(x,y)";
+            var question5Answer2 = "['cats', 'dogs', 'fish', 'cows']";
+            var question5Answer3 = "cool";
+            var question5Answer4 = ".innerHTML";
+            // do stuff
+            break;
+        default:
+            break;
     }
 
 
-    var question1Answer1 = "var grape = true";
-    var question1Answer2 = "[Apple, Orange, Bannanas]";
-    var question1Answer3 = "3";
-    var question1Answer4 = "5 + 6";
 
-    var question2Answer1 = "9870897";
-    var question2Answer2 = "['cats', 'dogs', 'fish', 'cows']";
-    var question2Answer3 = "false";
-    var question2Answer4 = "'jquery'";
-    
-    var question3Answer1 = "displaySolutions(x,y)";
-    var question3Answer2 = "['cats', 'dogs', 'fish', 'cows']";
-    var question3Answer3 = "false";
-    var question3Answer4 = "'jquery'";
 
-    var question4Answer1 = "displaySolutions(x,y)";
-    var question4Answer2 = "['cats', 'dogs', 'fish', 'cows']";
-    var question4Answer3 = "false";
-    var question4Answer4 = "'jquery'";
 
-    var question5Answer1 = "displaySolutions(x,y)";
-    var question5Answer2 = "['cats', 'dogs', 'fish', 'cows']";
-    var question5Answer3 = "cool";
-    var question5Answer4 = ".innerHTML";
+
+
+
+
+
+
 }
 
 function getCorrectAnswer(questionNumber, answerClick) {
